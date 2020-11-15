31252e97d685928c21b7bd75a8d9baabc5f89238
Konstantin Borovik <kb@lab5.ca>
2020-11-15 17:00:14 -0500

    Test self-hosted runner

--- terraform/cloud-init.sh
+++ terraform/cloud-init.sh
@@ -7,7 +7,7 @@ export DEBIAN_FRONTEND noninteractive
 apt -y update
 apt -y upgrade
 apt -y autoremove
-apt -y install apt-transport-https ca-certificates curl software-properties-common
+apt -y install apt-transport-https
 
 # Install Docker
 apt -y install docker.io
@@ -16,10 +16,3 @@ usermod -aG docker github
 
 # Add SSH Keys
 curl -fsSl https://github.com/kborovik.keys | tee -a /home/github/.ssh/authorized_keys
-
-# Install GitHub Actions Runner
-
-su github && cd
-mkdir actions-runner && cd actions-runner
-curl -O -L https://github.com/actions/runner/releases/download/v2.274.1/actions-runner-linux-x64-2.274.1.tar.gz
-tar xzf ./actions-runner-linux-x64-2.274.1.tar.gz && rm -f ./actions-runner-linux-x64-2.274.1.tar.gz
